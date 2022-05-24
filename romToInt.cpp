2484826aa31fbf04e5e707266a4123eeea205aae
Yanchong Xu <60683403+yx-xyc@users.noreply.github.com>
2022-05-24 17:40:36 -0400

    Add files via upload

---
+++ romToInt.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <map>
+#include <unordered_map>
+#include <printf.h>
+using namespace std;
+int romanToInt(string s) {
+    unordered_map<char, int> encoder = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+    int len = s.length();
+    int rom = 0;
+    for (int i=0;i<len-1;i++){
+        if (encoder[s[i]]<encoder[s[i+1]]){
+            rom -= encoder[s[i]];
+        } else {
+            rom += encoder[s[i]];
+        }
+    }
+    rom += encoder[s[len-1]];
+    return rom;
+}
+int main(void){
+    string a = "MCMXCIV";
+    romanToInt(a);
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+    while (dummy2!=nullptr){
+        cout << dummy2->val;
+        cout << endl;
+        dummy2 = dummy2->next;
+    }
